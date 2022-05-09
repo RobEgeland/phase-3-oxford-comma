0d08e85e77de88b1b88baff705c7e321b3168fa9
Robert Egeland <robegeland@yahoo.com>
2022-05-09 18:58:33 -0500

    commit tree 81679e629ce2a00bc741b3c25d03495960bcbda6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    and_array = []
+    if array.length() <= 2 && array.length() > 1
+        array[0] << " and"
+        array.join(" ")
+    elsif array.length() > 2
+        array[-1] = "and " + array[-1]
+        array.join(", ")
+    else
+        array.join()
+    end
+
+    #array.each_with_index do |word, index| 
+        #if index == 1
+            #word << "and"
+        #end
+        #and_array << word
+    #end
+    #binding.pry
+    #and_array.join(" ")
+    
 end
